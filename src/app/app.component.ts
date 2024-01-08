ef2d30458372bb6ed2e809c77d1bbbc892e02a76
Yannick Holzenkamp <yannick@holzenkamp.me>
2024-01-08 16:17:40 +0100

    add transloco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PizzaShopComponent } from './pizza-shop/pizza-shop.component';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { onBalConfigChange, BalConfigState } from '@baloise/design-system-components'
 
 @Component({
   standalone: true,
-  imports: [RouterModule, PizzaShopComponent],
+  imports: [RouterModule, PizzaShopComponent, TranslocoModule, TranslocoModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'basic-example-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'basic-example-app';
+export class AppComponent implements OnInit {
+
+  constructor(private transloco: TranslocoService) {}
+
+  ngOnInit(): void {
+    onBalConfigChange((config: BalConfigState) => {
+      this.transloco.setActiveLang(config.language)
+    })
+   }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideBaloiseDesignSystem } from '@baloise/design-system-components-angular/standalone'
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@ngneat/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    provideBaloiseDesignSystem(),
-    importProvidersFrom(HttpClientModule)
+    provideBaloiseDesignSystem({defaults: {region: 'CH', language: 'en', allowedLanguages: ['de', 'en']}}),
+    importProvidersFrom(HttpClientModule), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'de'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })
   ],
 };
